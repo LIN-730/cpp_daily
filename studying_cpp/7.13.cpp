a7cf830102135a39a2578a0ef7e2fae9edee491b
Lin <2740224106@qq.com>
2026-07-14 10:24:06 +0800

    7.13cpp学习，开始跟着c++ primer学

---
+++ studying_cpp/7.13.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+// ���ڲ��� 
+
+//int main()
+//{
+//   std::cout << "I love " << "Yoyo!";
+//}
+
+// ��ȡ������������������
+
+//int main()
+//{
+//	 int sum = 0,value = 0;
+//	 while (cin >> value)
+//		 sum += value;
+//	 cout << "Sum is: " << sum << endl; 
+//	 return 0;
+//}
+
